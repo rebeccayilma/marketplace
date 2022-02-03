fdd169a465a90049d8cfce0bc6fcfac30bf86e88
Rebecca <rebeccayilma@gmail.com>
2022-02-03 15:21:47 -0600

    product Controller

--- src/main/java/com/example/marketplace/product/ProductController.java
+++ src/main/java/com/example/marketplace/product/ProductController.java
@@ -1,5 +1,8 @@
 package com.example.marketplace.product;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class ProductController {
 
     public String product(String products) {
